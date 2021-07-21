317f1bc806c9329a4d1093a088b942b4a25d2ede
luoluoluoya <18684085840@163.com>
2021-07-21 23:48:19 +0800

    信息表示

---
+++ cmake-build-debug/CMakeFiles/system.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/system.dir/main.c.obj"
+  "libsystem.dll.a"
+  "system.exe"
+  "system.exe.manifest"
+  "system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
